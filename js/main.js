1fa46e460a6542e75bd4ad313bebd30741e92923
LeshVinov <90595946+LeshVinov@users.noreply.github.com>
2023-03-02 12:16:28 +0300

    feat: site done (with adapt and burger menu), without slider and form

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menuNav = document.querySelector('.header')
+let menuOpen = false;
+
+menuBtn.addEventListener('click', () => {
+    if (!menuOpen) {
+        menuBtn.classList.add('open');
+        menuNav.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuNav.classList.remove('open');
+        menuOpen = false;
+    }
+});
