0dd72d3c39ad5fce07bbb8999242ccc9e16022d7
LeshVinov <90595946+LeshVinov@users.noreply.github.com>
2023-03-03 13:08:08 +0300

    fix: fixed some strings in js... again

--- js/main.js
+++ js/main.js
@@ -46,11 +46,11 @@ const sliderFunc = () => {
         prevSlide(slides, countSlider, 'hero-slider__item--active')
         prevSlide(dots, countSlider, 'hero-slider-dots__item--active')
         countSlider++
-        sliderBlock.style.background = `url('../img/hero-bg-`+`${countSlider}`+`.jpg') no-repeat center / cover`
+        sliderBlock.style.background = `url('img/hero-bg-`+`${countSlider}`+`.jpg') no-repeat center / cover`
         if(countSlider >= slides.length) {
             countSlider = 0
         }
-        sliderBlock.style.background = `url('./img/hero-bg-`+`${countSlider}`+`.jpg') no-repeat center / cover`
+        sliderBlock.style.background = `url('img/hero-bg-`+`${countSlider}`+`.jpg') no-repeat center / cover`
 
         nextSlide(slides, countSlider, 'hero-slider__item--active')
         nextSlide(dots, countSlider, 'hero-slider-dots__item--active')
