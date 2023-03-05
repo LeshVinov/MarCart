7d47001b0bb6f3996643f6bc5774fd95d1abb907
LeshVinov <90595946+LeshVinov@users.noreply.github.com>
2023-03-05 15:09:21 +0300

    feat: service section done, sending form script done + some fixes in adaptation

--- js/main.js
+++ js/main.js
@@ -113,6 +113,102 @@ if (document.body.clientWidth>768){
     sliderFunc()
 } 
 
-// endSlider -------------------------------------------------------------------------
+// end Slider ------------------------------------------------------------------------
 
 // sending form ----------------------------------------------------------------------
+
+const form = document.getElementById('contact_form')
+const statusBlock = document.createElement('div')
+const loadText = 'Загрузка...'
+const errorText = 'Ошибка...'
+const validateErrorText = 'Поля заполнены неправильно!'
+const successText = 'Спасибо, наш менеджер с вами свяжется!'
+
+statusBlock.classList.add('contact-form__status')
+
+const validate = (formElements) => {
+    let success = true
+
+    formElements.forEach(input => {
+        if (input.name == 'contact-form_Name') {
+             if (input.value === '') {
+                 success = false
+             } else if (input.value.match(/[^а-яА-Я\^a-zA-Z\s]/g)) {
+                 success = false
+             }
+         } else if (input.name == 'contact-form_Phone') {
+             if (input.value === '') {
+                 success = false
+             } else if (input.value.match(/[^0-9\(\)\+\-]/g)) {
+                 success = false
+             }
+         } 
+     })
+
+
+
+    return success
+}
+
+const sendData = (data) => {
+    return fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST', 
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then(res => res.json())
+}
+
+const submitForm = () =>{
+    const formElements = form.querySelectorAll('.input')
+    const formData = new FormData(form)
+    const formBody = {}
+
+    statusBlock.textContent = loadText
+    form.append(statusBlock)
+
+    formData.forEach((val, key) => {
+        formBody[key] = val
+    })
+
+    // console.log('Хули ты отправил?')
+
+    if (validate(formElements)) {
+        sendData(formBody)
+        .then(data => {
+            statusBlock.textContent = successText
+
+            formElements.forEach(input => {
+                input.value = ''
+            })
+            setTimeout(() => {
+                statusBlock.remove()
+            }, 3000)
+            console.log(data);
+        })
+        .catch(error => {
+            statusBlock.textContent = errorText
+            
+        })
+    } else {
+        statusBlock.textContent = validateErrorText
+        setTimeout(() => {
+            statusBlock.remove()
+        }, 3000)
+    }
+}
+
+try {
+    if (!form) {
+        throw new Error('Верните форму')
+        
+    }
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        submitForm()
+    })
+} catch(error) {
+    console.log(error.message);
+}
